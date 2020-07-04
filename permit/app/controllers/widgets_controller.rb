c1661db64b57b7c46d5fc1872e760099a8e16929
Nick <nadams@dsc.net>
2020-07-04 17:20:48 +0100

    Includes all parameters in URLs

--- permit/app/controllers/widgets_controller.rb
+++ permit/app/controllers/widgets_controller.rb
@@ -1,6 +1,18 @@
 class WidgetsController < ApplicationController
   def index
     @widgets = Widget.all
+
+    order = params[:order]
+    if order == 'price'
+      @widgets = @widgets.order(:price)
+    elsif order == 'name'
+      @widgets = @widgets.order(:name)
+    end
+    
+    search_term = params[:search]
+    if search_term.present?
+      @widgets = @widgets.search(search_term)
+    end
   end
 
   def show
--- permit/app/models/widget.rb
+++ permit/app/models/widget.rb
@@ -1,3 +1,9 @@
 class Widget < ActiveRecord::Base
   validates :name, :price, presence: true
+
+  class << self
+    def search(criteria)
+      where arel_table[:name].matches("%#{criteria}%")
+    end
+  end
 end
